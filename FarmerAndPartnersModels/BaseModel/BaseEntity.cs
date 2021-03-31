225626b458a2f8dc170898f5afc8116c14e1293a
Sergei3190 <sergkosya@mail.ru>
2021-03-31 21:49:17 +0300

    добавлен базовый класс для моделей бд

---
+++ FarmerAndPartnersModels/BaseModel/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FarmerAndPartnersModels.BaseModel
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [Timestamp]
+        public byte[] TimeStamp { get; set; }
+        public override string ToString() => $" {nameof(Id)}: {Id}" +
+                                             $" {nameof(Name)}: {Name}";
+    }
+}
